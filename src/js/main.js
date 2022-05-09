af1627052ad5b9f61f8e8c2db54fdf38dd80e683
Liubov <kortcova@gmail.com>
2022-05-09 23:13:37 +0300

    first

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const myCarousel = new Carousel(document.querySelector(".carousel"), {
+  // Options
+});
+const testimonialsCarousel = new Carousel(
+  document.querySelector(".testimonials__carousel"),
+  {
+    // Options
+  }
+);
